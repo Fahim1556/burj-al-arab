cc0fc7f1f1dc4495ea05202f3a71c3faa9d58773
Fahim1556 <49098556+Fahim1556@users.noreply.github.com>
2020-09-14 13:05:48 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBH20f5iL-h2p8mvgbr4V2FBTCc3Q2_Uq4",
+    authDomain: "burj-al-arab-ae0b4.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-ae0b4.firebaseio.com",
+    projectId: "burj-al-arab-ae0b4",
+    storageBucket: "burj-al-arab-ae0b4.appspot.com",
+    messagingSenderId: "972303365374",
+    appId: "1:972303365374:web:f627016f53853d3dbfe1af"
+  };
+
+  export default firebaseConfig;
